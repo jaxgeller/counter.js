baf6ca8cabd178a33c2708863169d37448f50989
jaxgeller <jacksongeller@gmail.com>
2015-09-20 19:18:10 -0400

    Working on adding new options

--- main.js
+++ main.js
@@ -11,14 +11,16 @@ export default class Counter {
     this.tickIncrement = (to - from) / (this.duration/this.refresh);
     this.done = opts.done;
     this.max = opts.to
-    console.log(this)
+    console.log(`From ${from} to ${to} in ${this.duration}s with a refresh of ${this.refresh} and inc rate ${this.tickIncrement}`)
 
   }
 
   tick() {
-
     this.current += this.tickIncrement;
     this.selector.textContent = Math.round(this.current);
+
+    console.log(this.current)
+
 
     if (this.current < this.max)
       return setTimeout(this.tick.bind(this), this.refresh);
